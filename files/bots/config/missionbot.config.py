8a3b11666cdcae06dfd4f5c1d88e1085932c7d27
Sami Laine <sami.v.laine@gmail.com>
2022-09-18 16:06:31 +0000

    Store bot tokens using vault
    Don't worry, the previously committed token has been revoked :)

--- files/bots/config/missionbot.config.py
+++ files/bots/config/missionbot.config.py
@@ -1,6 +1,4 @@
-# Test bot token
 CFG_VERSION = 1
-TOKEN = 'NTI4MzI4NDg5ODE3MDc5ODQ5.DySx1Q.5jbIOppDCdAXzjGBapY9Gr1O-vs'
-# command@Foobar, general@Foobar
-CHANNEL_IDS = [867126221581451275, 235478105177718785]
+TOKEN = '{{ tokens["missionbot"] }}'
+CHANNEL_IDS = [564894057013313557]  # zeuses_upload
 REMOTE_FOLDER = '/srv/arma/server/link/mpmissions'
