2798c0b99702235bde5b55d8cd41491c2b1e6c56
Sami Laine <sami.v.laine@gmail.com>
2022-09-18 14:34:46 +0000

    Create a group that allows uploading mission files

--- files/bots/config/missionbot.config.py
+++ files/bots/config/missionbot.config.py
@@ -1,5 +1,6 @@
 # Test bot token
 CFG_VERSION = 1
 TOKEN = 'NTI4MzI4NDg5ODE3MDc5ODQ5.DySx1Q.5jbIOppDCdAXzjGBapY9Gr1O-vs'
+# command@Foobar, general@Foobar
 CHANNEL_IDS = [867126221581451275, 235478105177718785]
-REMOTE_FOLDER = None
+REMOTE_FOLDER = '/srv/arma/server/link/mpmissions'
