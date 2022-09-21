0ee0af1c10a9156a0eebfba699a293b9cacde752
Sami Laine <sami.v.laine@gmail.com>
2022-09-21 21:58:25 +0000

    bots: Add operationbot and backup cronjob

--- files/bots/config/missionbot.config.py
+++ files/bots/config/missionbot.config.py
@@ -1,5 +1,5 @@
 {{ ansible_managed | comment }}
 CFG_VERSION = 1
-TOKEN = '{{ tokens["missionbot"] }}'
+TOKEN = '{{ tokens.missionbot }}'
 CHANNEL_IDS = [564894057013313557]  # zeuses_upload
 REMOTE_FOLDER = '/srv/arma/server/link/mpmissions'
---
+++ files/bots/config/operationbot.secret.py
@@ -0,0 +1,12 @@
+{{ ansible_managed | comment }}
+VERSION = 1
+TOKEN = '{{ tokens.operationbot }}'
+COMMAND_CHAR = '!'
+# Gehock, Stroker
+ADMINS = [150625032656125952, 219897934370570240]
+ADMIN = ADMINS[0]
+DEBUG = False
+SIGNOFF_NOTIFY_USER = ADMINS[1]
+PLATOON_SIZE = "1PLT"
+WW2_MODS = """<https://steamcommunity.com/sharedfiles/filedetails/?id=2374862438>
+Collection above has mods required, > 22 GB total."""
