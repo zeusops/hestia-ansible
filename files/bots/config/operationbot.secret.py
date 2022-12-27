1c1d3148f50d073df94c0c10d0faf659b3e859d2
Sami Laine <sami.v.laine@gmail.com>
2022-12-27 21:58:58 +0000

    operationbot: Disable signoff ping

--- files/bots/config/operationbot.secret.py
+++ files/bots/config/operationbot.secret.py
@@ -6,7 +6,8 @@ COMMAND_CHAR = '!'
 ADMINS = [150625032656125952, 219897934370570240]
 ADMIN = ADMINS[0]
 DEBUG = False
-SIGNOFF_NOTIFY_USER = ADMINS[1]
+# Using the bot itself to disable the ping
+SIGNOFF_NOTIFY_USER = 528501657328156683
 PLATOON_SIZE = "1PLT"
 WW2_MODS = """<https://steamcommunity.com/sharedfiles/filedetails/?id=2374862438>
 Collection above has mods required, > 22 GB total."""
